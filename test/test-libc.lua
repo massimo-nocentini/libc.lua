b33498e61f2f90dc3bdd095cc937b3f479be7997
Massimo Nocentini <massimo.nocentini@gmail.com>
2022-12-28 16:17:46 +0100

    add a test using coroutines.

--- test/test-libc.lua
+++ test/test-libc.lua
@@ -161,15 +161,38 @@ function Test_pthread:test_pthread_equal_self ()
 end
 
 function Test_pthread:test_pthread_equal ()
-	
+
     local function A (main_thread)
         return libc.pthread.equal (libc.pthread.self (), main_thread)
     end
 
 	local pthread = libc.pthread.checked_create 'pthread_create failed.' (A, libc.pthread.self ())
     lambda.o { lu.assertFalse, libc.pthread.checked_join 'pthread_join failed.' } (pthread)
-    
+
 end
+
+function Test_pthread:test_pthread_coro ()
+
+    local one, two = 1, 2
+
+    local coroA =  coroutine.create (function () while true do print ('A ready'); coroutine.yield (one) end end)
+    local coroB =  coroutine.create (function () while true do print ('B ready'); coroutine.yield (two) end end)
+    
+    local f = function (coro, j)
+        for i = 1, j do
+            local flag, v = coroutine.resume (coro)
+            if flag and v ~= nil then print ('C', v) end
+        end
+    end
+
+	local pthread_one = libc.pthread.checked_create 'pthread_create failed.' (f, coroA, 100)
+    local pthread_two = libc.pthread.checked_create 'pthread_create failed.' (f, coroB, 100)
+    
+    libc.pthread.checked_join 'pthread_join failed.' (pthread_one)
+    libc.pthread.checked_join 'pthread_join failed.' (pthread_two)
+
+end
+
 
 --------------------------------------------------------------------------------
 
